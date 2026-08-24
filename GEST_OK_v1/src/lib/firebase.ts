34c68dd71609f01d4a31c53c3800717fb8b87fe7
rcamposp1973 <rcampos@pulsocontable.cl>
2026-08-24 15:57:47 -0400

    Delete GEST_OK_v1 directory

--- GEST_OK_v1/src/lib/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
-
-const app = initializeApp(firebaseConfig);
-// CRITICAL: The app will break without the firestoreDatabaseId in getFirestore
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth();
